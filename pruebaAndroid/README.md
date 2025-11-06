16e6556d5b5b7a36d19a637eb538c99a42014493
danielagsc0806 <danielagsc0806@gmail.com>
2025-11-05 23:33:26 -0600

    Actualizacion funcional 2

--- pruebaAndroid/README.md
+++ pruebaAndroid/README.md
@@ -9,15 +9,22 @@ npm install appium appium-uiautomator2-driver wd --save-dev
 Android SDK Command Line Tools:
 https://developer.android.com/studio#cmdline-tools
 
+Ver lista de systemas disponibles:
+sdkmanager --list | Select-String "system-images"
+
+Ver lista de dispositivos disponibles:
+avdmanager list devices
+
 Instalar imagen:
 sdkmanager "system-images;android-33;google_apis;x86_64"
+sdkmanager "system-images;android-33;google_apis_playstore;x86_64"
 
 Crear emulador:
 avdmanager create avd -n testEmu -k "system-images;android-33;google_apis;x86_64"
-Emulador Samsung:
-avdmanager create avd -n GalaxyS10 -k "system-images;android-33;google_apis;x86_64"
 Emulador pixel:
 avdmanager create avd -n testPixel -k "system-images;android-33;google_apis;x86_64" --device "pixel_4"
+Emulador prueba 1:
+avdmanager create avd -n testML -k "system-images;android-33;google_apis_playstore;x86_64" --device "pixel_5"
 
 Ver lista de emuladores:  emulator -list-avds
 
@@ -29,7 +36,7 @@ Driver Android:
 appium driver install uiautomator2
 
 Instalacion app mercado libre:
-adb install apps\com.mercadolibre.apk
+adb install apps\com.mercadolibre_apkmirror.com.apk
 
 Verificar pantalla de emulador:
 adb shell dumpsys window | Select-String "mCurrentFocus","mFocusedApp"
--- pruebaAndroid/scripts/test_mercado.rb
+++ pruebaAndroid/scripts/test_mercado.rb
@@ -9,22 +9,45 @@ opts = {
 }
 
 driver = Appium::Driver.new(opts, true)
+wait = Selenium::WebDriver::Wait.new(timeout: 10)
 driver.start_driver
 driver.screenshot('screenshots/inicio.png')
-
 puts "✅ App Mercado Libre abierta correctamente"
 
 # Ejemplo: buscar un producto
-search_container = driver.find_element(:id, "com.mercadolibre:id/ui_components_action_bar_search_field")
+search_container = driver.find_element(:id, "com.mercadolibre:id/ui_components_toolbar_search_field")
 search_container.click
 driver.screenshot('screenshots/busqueda.png');
-sleep 1
+puts "✅ hacer busqueda correctamente"
 
 
-search_box = driver.find_element(:id, "com.mercadolibre:id/search_input_edittext")
+search_box = driver.find_element(:id, "com.mercadolibre:id/autosuggest_input_search")
 search_box.send_keys("playstation 5")
-sleep 1
 driver.press_keycode(66) # Enter
+wait.until {driver.find_element(:id, "com.mercadolibre:id/search_main_content").displayed?}
 driver.screenshot('screenshots/busqueda_playstation5.png');
-sleep 5
-driver.quit
+puts "✅ se muestran resultados de busqueda correctamente"
+
+filter_button = driver.find_element(:xpath, '(//android.widget.LinearLayout[@resource-id="com.mercadolibre:id/appbar_content_layout"])[1]/android.widget.LinearLayout')
+filter_button.click
+driver.screenshot('screenshots/filtros.png');
+puts "✅ se abre seccion de filtros correctamente"
+
+frame_filter = driver.find_element(:xpath, '//android.widget.FrameLayout[@resource-id="com.mercadolibre:id/andes_bottom_sheet_frame_view"]/android.widget.FrameLayout')
+wait.until {frame_filter.displayed?}
+puts "✅ el frame de los filtros esta visible"
+#####################################################condition new#############################################
+category_condition = driver.find_element(:xpath, '//android.view.View[@content-desc="Condición"]')
+category_condition.click
+puts "✅ boton condicion clickeado"
+
+wait.until {driver.find_element(:xpath, '//android.view.View[@resource-id="ITEM_CONDITION"]').displayed?}
+puts "✅ se muestra la seccion de condicion de los productos"
+
+condition_new = driver.find_element(:xpath, '//android.widget.ToggleButton[@resource-id="ITEM_CONDITION-2230284"]')
+condition_new.click
+driver.screenshot('screenshots/filtro_condicion_nuevo.png');
+puts "✅ filtro condicion nuevo seleccionado"
+#######################################################order by high price##########################################
+
+driver.driver_quit
